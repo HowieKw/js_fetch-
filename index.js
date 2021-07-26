ce28a1a3d56343094aedab132cde37be00cac787
Aysan Isayo <aisayo@icloud.com>
2021-07-26 09:16:16 -0700

    changed solution

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-const BASE_URL = "https://www.openbrewerydb.org/";
+const BASE_URL = "https://api.openbrewerydb.org/breweries";
+const container = document.getElementById('brewery-list')
 
+function renderBrew(brew) {
+  const card = document.createElement("div");
+  card.id = `brew-${brew.id}`;
+
+  const header = document.createElement("h3");
+  header.id = "brew-header";
+  header.textContent = brew.name;
+
+  const p = document.createElement("p");
+  p.id = "brew-info";
+  p.textContent = `
+    Type: ${brew.brewery_type} - 
+    City: ${brew.city} - 
+    State: ${brew.state}
+    `;
+
+  card.append(header, p)
+  container.appendChild(card)
+}
+
+function fetchBrews() {
+  fetch(BASE_URL)
+    .then((resp) => resp.json())
+    .then((brews) => {
+      brews.forEach(brew => renderBrew(brew));
+    });
+}
+
+function init() {
+  fetchBrews();
+}
+
+init();
